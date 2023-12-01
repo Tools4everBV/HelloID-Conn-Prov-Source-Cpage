4fc5311e3ec5eea67128f2030071b6d7d20332b1
Patrick HORYN <71121140+patrickhoryn@users.noreply.github.com>
2023-12-01 19:08:35 +0100

    Update Departments.ps1

--- Departments.ps1
+++ Departments.ps1
@@ -1,31 +1,34 @@
 $config = ConvertFrom-Json $configuration
-try{
-    [System.Reflection.Assembly]::LoadWithPartialName("System.Data.OracleClient")| out-null
-    $conn = New-Object System.Data.Odbc.OdbcConnection
+try {
+    [System.Reflection.Assembly]::LoadWithPartialName("System.Data.OracleClient") | out-null
+    # Creating a new object
+    $conn = [System.Data.Odbc.OdbcConnection]::new()
     $conn.connectionstring = $config.dsn
     $conn.open()
 
-    ### Unités Fonctionnelles
+    # Querying UF
     $sql = "SELECT DISTINCT * FROM UFP"
-    $cmd = New-Object system.Data.Odbc.OdbcCommand($sql,$conn)
-    $da = New-Object system.Data.Odbc.OdbcDataAdapter($cmd)
-    $dt = New-Object system.Data.datatable
+    $cmd = [System.Data.Odbc.OdbcCommand]::new($sql, $conn)
+    $da = [System.Data.Odbc.OdbcDataAdapter]::new($cmd)
+    $dt = [System.Data.datatable]::new()
     $null = $da.fill($dt)
-    $Departments += $dt
-    $Departments = $Departments | Select-Object -Property * -ExcludeProperty RowError,RowState,Table,ItemArray,HasErrors 
+    $departments += $dt
+    $departments = $departments  | Select-Object -Property * -ExcludeProperty RowError,RowState,Table,ItemArray,HasErrors
     $conn.close()
-    $result = @()
-    
-    foreach($item in $Departments){
+
+    $result = [System.Collections.Generic.List[PSCustomObject]]::new()
+
+    foreach($item in $departments){
         $department = @{
-            ExternalId=$item.NUFPUF
-            DisplayName=$item.LBRPUF
-            Name=$item.LBRPUF 
+            ExternalId  = $item.NUFPUF
+            DisplayName = $item.LBRPUF
+            Name        = $item.LBRPUF
         }
+        $result.Add($department)
         Write-Output ($department | ConvertTo-Json -Depth 50)
-        $result += $department 
     }
-Write-Verbose -Verbose "$($result.count) departements imported"
+    Write-Verbose -Verbose "$($result.count) departements imported"
 }catch{
-    Write-verbose -verbose "Error during UF importation - $error[0].Exception.Message"
+    
+    Write-verbose -verbose "Error during UF importation - $($_.Exception.Message)"
 }
